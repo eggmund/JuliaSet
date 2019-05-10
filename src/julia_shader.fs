4b52b7f78e0411060d439ba27deb97e8ca554f90
eggmund <joshuacolclough2@googlemail.com>
2019-05-10 13:32:30 +0100

    Implemented as shader.

---
+++ src/julia_shader.fs
@@ -0,0 +1,38 @@
+#version 330
+#extension GL_ARB_explicit_uniform_location : enable
+
+layout(location = 0) uniform vec2 screen_dims;
+layout(location = 1) uniform int max_iterations;
+layout(location = 2) uniform vec2 c;  // c.x = real, c.y = imagin
+layout(location = 3) uniform vec2 offset;
+layout(location = 4) uniform float zoom;
+
+
+
+// Output fragment color
+out vec4 final_color;
+
+vec4 get_color(float norm) {
+   norm = pow(norm, 1.0/3.0);
+   return vec4(norm, norm/3, (norm/2) + 127, 255);
+}
+
+int julia() {
+   float zx = ((gl_FragCoord.x + offset.x)/screen_dims.x) * 2.5;
+   float zy = ((screen_dims.y - gl_FragCoord.y + offset.y)/screen_dims.y) * 1.5;
+   int iterations = 0;
+   
+   while (zx * zx + zy * zy < 4.0 && iterations < max_iterations) {
+      float xtemp = zx * zx - zy * zy;
+      zy = 2.0 * zx * zy + c.y;
+      zx = xtemp + c.x;
+
+      iterations++;
+   }
+
+   return iterations;
+}
+
+void main() {
+   final_color = get_color(float(julia())/float(max_iterations));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,17 @@
 use raylib::{Color, Vector2, RaylibHandle};
 
-const MAX_ITERATIONS: u64 = 170;
+const MAX_ITERATIONS: u64 = 200;
 const SCREEN_W: i32 = 1280;
 const SCREEN_H: i32 = 720;
 const HALF_SCREEN_W: i32 = SCREEN_W/2;
 const HALF_SCREEN_H: i32 = SCREEN_H/2;
 
-#[inline]
-fn julia(x: f64, y: f64, cx: f64, cy: f64) -> u64 {
-   let mut zx = (x/SCREEN_W as f64) * 2.5;
-   let mut zy = (y/SCREEN_H as f64) * 1.5;
-   let mut iterations: u64 = 0;
-   
-   while zx * zx + zy * zy < 4.0 && iterations < MAX_ITERATIONS {
-      let xtemp = zx * zx - zy * zy;
-      zy = 2.0 * zx * zy + cy;
-      zx = xtemp + cx;
+const SHADER_SCREEN_DIMS_LOC: i32 = 0;
+const SHADER_MAX_ITERATIONS_LOC: i32 = 1;
+const SHADER_C_LOC: i32 = 2;
+const SHADER_OFFSET_LOC: i32 = 3;
+const SHADER_ZOOM_LOC: i32 = 4;
 
-      iterations += 1;
-   }
-
-   iterations
-}
-
-/*
-struct Pixel {
-   x: i32,
-   y: i32,
-   col: Color,
-}
-*/
-
-#[inline]
-fn get_colour(norm: f64) -> Color {
-   let scaled = (norm * 255.0).floor() as u8;
-   Color { r: scaled, g: scaled/3, b: (scaled/2) + 127, a: 255}
-}
-
-fn run_julia(rl: &RaylibHandle, offset: Vector2, zoom: f64, cx: f64, cy: f64) {
-   for i in 0..SCREEN_W {
-      for j in 0..SCREEN_H {
-         let iter = julia((i as f64 + offset.x as f64) * zoom, (j as f64 + offset.y as f64) * zoom, cx, cy);
-         let norm: f64 = iter as f64/MAX_ITERATIONS as f64;
-
-         rl.draw_pixel(i, j, get_colour(norm.powf(1.0/3.0)));
-      }
-   }
-}
 
 fn main() {
    let rl = raylib::init()
@@ -54,26 +19,34 @@ fn main() {
       .title("Julia")
       .build();
  
-   rl.set_target_fps(60);
-
    let mut cx: f64 = -0.77322;
    let mut cy: f64 = 0.15868;
+   let offset = Vector2 { x: -HALF_SCREEN_W as f32, y: -HALF_SCREEN_H as f32 };
    let zoom: f64 = 1.3;
 
-
-   //let mut pixels: Vec<Pixel> = vec![];
+   let mut shader = rl.load_shader("", "src/julia_shader.fs");
+   rl.set_shader_value(&mut shader, SHADER_SCREEN_DIMS_LOC, &[SCREEN_W as f32, SCREEN_H as f32]);
+   rl.set_shader_value_i(&mut shader, SHADER_MAX_ITERATIONS_LOC, &[MAX_ITERATIONS as i32]);
+   rl.set_shader_value(&mut shader, SHADER_C_LOC, &[cx as f32, cy as f32]);
+   rl.set_shader_value(&mut shader, SHADER_OFFSET_LOC, &[offset.x, offset.y]);
+   rl.set_shader_value(&mut shader, SHADER_ZOOM_LOC, &[zoom as f32]);
 
    while !rl.window_should_close() {
       let dt = rl.get_frame_time();
       cx += -0.001 * dt as f64;
       cy += -0.0001 * dt as f64;
+      rl.set_shader_value(&mut shader, SHADER_C_LOC, &[cx as f32, cy as f32]);
 
 
       rl.begin_drawing();
       rl.clear_background(Color::BLACK);
 
-      run_julia(&rl, Vector2 { x: -HALF_SCREEN_W as f32, y: -HALF_SCREEN_H as f32 }, zoom, cx, cy);
+      //run_julia(&rl, Vector2 { x: -HALF_SCREEN_W as f32, y: -HALF_SCREEN_H as f32 }, zoom, cx, cy);
+      rl.begin_shader_mode(&shader);
+      rl.draw_rectangle(0, 0, SCREEN_W, SCREEN_H, Color::BLACK);
+      rl.end_shader_mode();
 
+      rl.draw_fps(10, SCREEN_H - 25);
       rl.draw_text(format!("frame time: {:.5}\ncx: {:.5}\ncy: {:.5}", rl.get_frame_time(), cx, cy).as_str(), 10, 10, 20, Color::RAYWHITE);
       rl.end_drawing();
    }
