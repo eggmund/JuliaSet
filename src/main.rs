5a2a628c5801ca595d30454b9088897fb3dd7be5
eggmund <joshuacolclough2@googlemail.com>
2019-05-10 13:03:26 +0100

    Added colour

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use raylib::{Color, Vector2};
+use raylib::{Color, Vector2, RaylibHandle};
 
 const MAX_ITERATIONS: u64 = 170;
 const SCREEN_W: i32 = 1280;
@@ -23,26 +23,29 @@ fn julia(x: f64, y: f64, cx: f64, cy: f64) -> u64 {
    iterations
 }
 
+/*
 struct Pixel {
    x: i32,
    y: i32,
    col: Color,
 }
+*/
 
-fn run_julia(offset: Vector2, cx: f64, cy: f64) -> Vec<Pixel> {
-   let mut out: Vec<Pixel> = vec![];
+#[inline]
+fn get_colour(norm: f64) -> Color {
+   let scaled = (norm * 255.0).floor() as u8;
+   Color { r: scaled, g: scaled/3, b: (scaled/2) + 127, a: 255}
+}
 
+fn run_julia(rl: &RaylibHandle, offset: Vector2, zoom: f64, cx: f64, cy: f64) {
    for i in 0..SCREEN_W {
       for j in 0..SCREEN_H {
-         let iter = julia(i as f64 + offset.x as f64, j as f64 + offset.y as f64, cx, cy);
-         let norm_val: f64 = iter as f64/MAX_ITERATIONS as f64;
-         let norm = (norm_val * 255.0).floor() as u8;
+         let iter = julia((i as f64 + offset.x as f64) * zoom, (j as f64 + offset.y as f64) * zoom, cx, cy);
+         let norm: f64 = iter as f64/MAX_ITERATIONS as f64;
 
-         out.push(Pixel { x: i, y: j, col: Color { r: norm, g: norm, b: norm, a: 255 }});
+         rl.draw_pixel(i, j, get_colour(norm.powf(1.0/3.0)));
       }
    }
-
-   out
 }
 
 fn main() {
@@ -53,30 +56,25 @@ fn main() {
  
    rl.set_target_fps(60);
 
-   let mut rendered = false;
-   let mut cx: f64 = -0.8;
-   let mut cy: f64 = 0.156;
+   let mut cx: f64 = -0.77322;
+   let mut cy: f64 = 0.15868;
+   let zoom: f64 = 1.3;
 
 
-   let mut pixels: Vec<Pixel> = vec![];
+   //let mut pixels: Vec<Pixel> = vec![];
+
    while !rl.window_should_close() {
       let dt = rl.get_frame_time();
-      cx += 0.005 * dt as f64;
-      cy += 0.001 * dt as f64;
-      rendered = false;
+      cx += -0.001 * dt as f64;
+      cy += -0.0001 * dt as f64;
 
-      if !rendered {
-         pixels = run_julia(Vector2 { x: -HALF_SCREEN_W as f32, y: -HALF_SCREEN_H as f32 }, cx, cy);
-         rendered = true;
-      }
 
       rl.begin_drawing();
       rl.clear_background(Color::BLACK);
-      for p in pixels.iter() {
-         rl.draw_pixel(p.x, p.y, p.col);
-      }
 
-      rl.draw_fps(10, 10);
+      run_julia(&rl, Vector2 { x: -HALF_SCREEN_W as f32, y: -HALF_SCREEN_H as f32 }, zoom, cx, cy);
+
+      rl.draw_text(format!("frame time: {:.5}\ncx: {:.5}\ncy: {:.5}", rl.get_frame_time(), cx, cy).as_str(), 10, 10, 20, Color::RAYWHITE);
       rl.end_drawing();
    }
 }
