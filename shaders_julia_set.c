552edfe4919f371c031070bfb98afadfef83175a
eggmund <joshuacolclough2@googlemail.com>
2019-05-13 13:12:57 +0100

    Cleaned it up a bit.

--- shaders_julia_set.c
+++ shaders_julia_set.c
@@ -1,19 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [shaders] example - Render julia sets using a shader.
-*
-*   NOTE: This example requires raylib OpenGL 3.3 or ES2 versions for shaders support,
-*         OpenGL 1.1 does not support shaders, recompile raylib to OpenGL 3.3 version.
-*
-*   NOTE: Shaders used in this example are #version 330 (OpenGL 3.3).
-*
-*   This example has been created using raylib 2.5 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Author: eggmund (https://github.com/eggmund)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 #include <string.h> // For memcpy
 
@@ -23,7 +7,7 @@ const float AUTO_SPEED = 0.0005;
 // A few good julia sets
 const float POINTS_OF_INTEREST[6][2] =
 { 
-    {-0.348827, 0.607167},
+    {-0.35309, 0.60291},
     {-0.786268, 0.169728},
     {-0.8, 0.156},
     {0.285, 0.0},
@@ -31,8 +15,7 @@ const float POINTS_OF_INTEREST[6][2] =
     {-0.70176, -0.3842},
 };
 
-int main()
-{
+int main() {
     // Initialization
     //--------------------------------------------------------------------------------------
     int screenWidth = 1280;
@@ -43,13 +26,15 @@ int main()
     // If julia set is rendered for this frame.
     bool rendered = false;
 
-    bool showControls = true;
-
     // Multiplier of speed to change c value. Set to 3 to start off with.
     int incrementSpeed = 3;
 
     // Offset and zoom to draw the julia set at. (centered on screen and 2 times smaller)
     float offset[2] = { -(float)screenWidth/2, -(float)screenHeight/2 }; 
+
+    offset[0] += -270.0;
+    offset[1] += 200.0;
+
     float zoom = 1.6;
 
     // c constant to use in z^2 + c
@@ -78,42 +63,20 @@ int main()
     RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
     
     SetTargetFPS(60);                       // Set the window to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())            // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-
-        // Get input
-        //----------------------------------------------------------------------------------
-
-        // Press 0 - 4 to reset c to a point of interest.
-        if (IsKeyPressed(KEY_ONE) || IsKeyPressed(KEY_TWO) || IsKeyPressed(KEY_THREE) || IsKeyPressed(KEY_FOUR) || IsKeyPressed(KEY_FIVE) || IsKeyPressed(KEY_SIX))
-        {
-            if (IsKeyPressed(KEY_ONE))
-            {
+    while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_ONE) || IsKeyPressed(KEY_TWO) || IsKeyPressed(KEY_THREE) || IsKeyPressed(KEY_FOUR) || IsKeyPressed(KEY_FIVE) || IsKeyPressed(KEY_SIX)) {
+            if (IsKeyPressed(KEY_ONE)) {
                 memcpy(c, &POINTS_OF_INTEREST[0], 8);
-            }
-            else if (IsKeyPressed(KEY_TWO))
-            {
+            } else if (IsKeyPressed(KEY_TWO)) {
                 memcpy(c, &POINTS_OF_INTEREST[1], 8);
-            }
-            else if (IsKeyPressed(KEY_THREE))
-            {
+            } else if (IsKeyPressed(KEY_THREE)) {
                 memcpy(c, &POINTS_OF_INTEREST[2], 8);
-            }
-            else if (IsKeyPressed(KEY_FOUR))
-            {
+            } else if (IsKeyPressed(KEY_FOUR)) {
                 memcpy(c, &POINTS_OF_INTEREST[3], 8);
-            }
-            else if (IsKeyPressed(KEY_FIVE))
-            {
+            } else if (IsKeyPressed(KEY_FIVE)) {
                 memcpy(c, &POINTS_OF_INTEREST[4], 8);
-            }
-            else if (IsKeyPressed(KEY_SIX))
-            {
+            } else if (IsKeyPressed(KEY_SIX)) {
                 memcpy(c, &POINTS_OF_INTEREST[5], 8);
             }
             SetShaderValue(shader, cLoc, c, UNIFORM_VEC2);
@@ -121,34 +84,23 @@ int main()
         }
 
         // Press "r" to stop changing c
-        if (IsKeyPressed(KEY_R))
-        {
+        if (IsKeyPressed(KEY_R)) {
             incrementSpeed = 0;
-        }
-
-        // Toggle whether or not to show controls
-        if (IsKeyPressed(KEY_H))
-        {
-            showControls = !showControls;
         }
 
         // Scroll to change c increment speed.
         int mouseMv = GetMouseWheelMove();  // Get the amount the mouse has moved this frame
-        if (mouseMv != 0)
-        {
-            if (IsKeyDown(KEY_LEFT_SHIFT))
-            {
+        if (mouseMv != 0) {
+            if (IsKeyDown(KEY_LEFT_SHIFT)) {
                 incrementSpeed += mouseMv * 10;
             }
-            else
-            {
+            else {
                 incrementSpeed += mouseMv;
             }
             rendered = false;
         }
 
-        if (incrementSpeed != 0)
-        {
+        if (incrementSpeed != 0) {
             float amount = GetFrameTime() * incrementSpeed * AUTO_SPEED;
             c[0] += amount;
             c[1] += amount;
@@ -158,17 +110,12 @@ int main()
             rendered = false;
         }
         
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(BLACK);  // Clear the screen of the previous frame.
             
             // If the c value has changed, redraw the julia set using the shader, onto the render texture.
-            if (!rendered)
-            {
+            if (!rendered) {
                 BeginTextureMode(target);   // Enable drawing to texture
 
                     ClearBackground(BLACK); // Clear the last frame drawn on the texture.
@@ -188,26 +135,16 @@ int main()
             
             // Print information.
             DrawText( FormatText("cx: %f\ncy: %f\nspeed: %d", c[0], c[1], incrementSpeed), 10, 10, 20, RAYWHITE );
-
-            if (showControls)
-            {
-                DrawText("Press keys 1 - 6 to change point of interest.", 10, screenHeight - 88, 20, RAYWHITE);
-                DrawText("Use the scroll wheel to auto increment the c value. Hold shift while scrolling to increase speed by 10.", 10, screenHeight - 66, 20, RAYWHITE);
-                DrawText("Press 'r' to reset speed.", 10, screenHeight - 44, 20, RAYWHITE);
-                DrawText("Press 'h' to hide these controls.", 10, screenHeight - 22, 20, RAYWHITE);
-            }
+            DrawFPS(10, screenHeight - 30);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
     UnloadShader(shader);           // Unload shader
     UnloadRenderTexture(target);    // Unload render texture
 
     CloseWindow();                  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
